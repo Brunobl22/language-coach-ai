789306f9b2f82d23c9e4479ce798e2bdd394a5af
Brunobl22 <104857198+Brunobl22@users.noreply.github.com>
2026-06-17 07:41:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if st.sidebar.button("Entrar / Cadastrar"):
 
     else:
         st.sidebar.error("Senha incorreta")
-     if "logado" not in st.session_state:
+   if "logado" not in st.session_state:
         st.session_state.logado = False
 
    if not st.session_state.logado:
